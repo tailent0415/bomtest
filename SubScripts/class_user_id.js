9e960b3aebfda5b91da1d0bc8e2fe51b8099ec3c
tailent0415 <40162330+tailent0415@users.noreply.github.com>
2019-01-16 11:06:02 +0800

    test
    test

--- SubScripts/class_user_id.js
+++ SubScripts/class_user_id.js
@@ -1,11 +1,13 @@
 class UserID {
 	
 	static SetID( id ) {
+		alert( id );
 		this.user_id = id;
 		return this.user_id;
 	}
 
 	static GetID() {
+		alert( this.user_id );
 		return this.user_id;
 	}
 	
